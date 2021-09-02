ddc52cb41e74e65ad55dc40c7cac9bc5979ebbbb
William Kennedy <bill@ardanlabs.com>
2021-09-02 11:17:18 -0400

    6.4-WebFramework

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -2,9 +2,9 @@ package testgrp
 
 import (
 	"context"
-	"encoding/json"
 	"net/http"
 
+	"github.com/ardanlabs/service/foundation/web"
 	"go.uber.org/zap"
 )
 
@@ -24,5 +24,5 @@ func (h Handlers) Test(ctx context.Context, w http.ResponseWriter, r *http.Reque
 	statusCode := http.StatusOK
 	h.Log.Infow("readiness", "statusCode", statusCode, "method", r.Method, "path", r.URL.Path, "remoteaddr", r.RemoteAddr)
 
-	return json.NewEncoder(w).Encode(status)
+	return web.Respond(ctx, w, status, http.StatusOK)
 }
