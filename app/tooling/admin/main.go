242cf97b989fe625b9ff74162d13501361706d3f
William Kennedy <bill@ardanlabs.com>
2021-09-03 12:05:05 -0400

    8.1-JWT

---
+++ app/tooling/admin/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"os"
+)
+
+func main() {
+	err := genKey()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+// genKey creates an x509 private/public key for auth tokens.
+func genKey() error {
+
+	// Generate a new private key.
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return err
+	}
+
+	// Create a file for the private key information in PEM form.
+	privateFile, err := os.Create("private.pem")
+	if err != nil {
+		return fmt.Errorf("creating private file: %w", err)
+	}
+	defer privateFile.Close()
+
+	// Construct a PEM block for the private key.
+	privateBlock := pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	}
+
+	// Write the private key to the private key file.
+	if err := pem.Encode(privateFile, &privateBlock); err != nil {
+		return fmt.Errorf("encoding to private file: %w", err)
+	}
+
+	// =========================================================================
+
+	// Marshal the public key from the private key to PKIX.
+	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return fmt.Errorf("marshaling public key: %w", err)
+	}
+
+	// Create a file for the public key information in PEM form.
+	publicFile, err := os.Create("public.pem")
+	if err != nil {
+		return fmt.Errorf("creating public file: %w", err)
+	}
+	defer publicFile.Close()
+
+	// Construct a PEM block for the public key.
+	publicBlock := pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: asn1Bytes,
+	}
+
+	// Write the public key to the private key file.
+	if err := pem.Encode(publicFile, &publicBlock); err != nil {
+		return fmt.Errorf("encoding to public file: %w", err)
+	}
+
+	fmt.Println("private and public key files generated")
+	return nil
+}
--- makefile
+++ makefile
@@ -6,10 +6,17 @@ SHELL := /bin/bash
 # expvarmon -ports=":4000" -vars="build,requests,goroutines,errors,panics,mem:memstats.Alloc"
 # hey -m GET -c 100 -n 10000 http://localhost:3000/v1/test
 
+# To generate a private/public key PEM file.
+# openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:2048
+# openssl rsa -pubout -in private.pem -out public.pem
+
 # ==============================================================================
 
 run:
 	go run app/services/sales-api/main.go | go run app/tooling/logfmt/main.go
+
+admin:
+	go run app/tooling/admin/main.go
 
 # ==============================================================================
 # Building containers
