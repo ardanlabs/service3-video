f09124f0408f1cfba2b391f0bc55dddb0383e381
William Kennedy <bill@ardanlabs.com>
2021-09-07 13:17:07 -0400

    11.2-DBMigrations

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -19,11 +19,38 @@ import (
 
 func main() {
 	err := migrate()
-
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func seed() error {
+	cfg := database.Config{
+		User:         "postgres",
+		Password:     "postgres",
+		Host:         "localhost",
+		Name:         "postgres",
+		MaxIdleConns: 0,
+		MaxOpenConns: 0,
+		DisableTLS:   true,
+	}
+
+	db, err := database.Open(cfg)
+	if err != nil {
+		return fmt.Errorf("connect database: %w", err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := schema.Seed(ctx, db); err != nil {
+		return fmt.Errorf("seed database: %w", err)
+	}
+
+	fmt.Println("seed data complete")
+	return nil
 }
 
 func migrate() error {
@@ -51,7 +78,8 @@ func migrate() error {
 	}
 
 	fmt.Println("migrations complete")
-	return nil
+
+	return seed()
 }
 
 func genToken() error {
--- business/data/schema/schema.go
+++ business/data/schema/schema.go
@@ -15,6 +15,9 @@ var (
 	//go:embed sql/schema.sql
 	schemaDoc string
 
+	//go:embed sql/seed.sql
+	seedDoc string
+
 	//go:embed sql/delete.sql
 	deleteDoc string
 )
@@ -33,6 +36,28 @@ func Migrate(ctx context.Context, db *sqlx.DB) error {
 
 	d := darwin.New(driver, darwin.ParseMigrations(schemaDoc))
 	return d.Migrate()
+}
+
+// Seed runs the set of seed-data queries against db. The queries are ran in a
+// transaction and rolled back if any fail.
+func Seed(ctx context.Context, db *sqlx.DB) error {
+	if err := database.StatusCheck(ctx, db); err != nil {
+		return fmt.Errorf("status check database: %w", err)
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	if _, err := tx.Exec(seedDoc); err != nil {
+		if err := tx.Rollback(); err != nil {
+			return err
+		}
+		return err
+	}
+
+	return tx.Commit()
 }
 
 // DeleteAll runs the set of Drop-table queries against db. The queries are ran in a
