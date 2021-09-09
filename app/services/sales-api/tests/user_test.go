8094bc17dafa41e5a1951afb25b81aa533b28050
William Kennedy <bill@ardanlabs.com>
2021-09-09 12:42:06 -0400

    14.3-RestAPI

---
+++ app/services/sales-api/tests/user_test.go
@@ -0,0 +1,100 @@
+package tests
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/ardanlabs/service/app/services/sales-api/handlers"
+	"github.com/ardanlabs/service/business/data/tests"
+)
+
+// UserTests holds methods for each user subtest. This type allows passing
+// dependencies for tests while still providing a convenient syntax when
+// subtests are registered.
+type UserTests struct {
+	app        http.Handler
+	userToken  string
+	adminToken string
+}
+
+// TestUsers is the entry point for testing user management functions.
+func TestUsers(t *testing.T) {
+	test := tests.NewIntegration(
+		t,
+		tests.DBContainer{
+			Image: "postgres:13-alpine",
+			Port:  "5432",
+			Args:  []string{"-e", "POSTGRES_PASSWORD=postgres"},
+		},
+	)
+	t.Cleanup(test.Teardown)
+
+	shutdown := make(chan os.Signal, 1)
+	tests := UserTests{
+		app: handlers.APIMux(handlers.APIMuxConfig{
+			Shutdown: shutdown,
+			Log:      test.Log,
+			Auth:     test.Auth,
+			DB:       test.DB,
+		}),
+		userToken:  test.Token("user@example.com", "gophers"),
+		adminToken: test.Token("admin@example.com", "gophers"),
+	}
+
+	t.Run("getToken404", tests.getToken404)
+	t.Run("getToken200", tests.getToken200)
+}
+
+// getToken401 ensures an unknown user can't generate a token.
+func (ut *UserTests) getToken404(t *testing.T) {
+	r := httptest.NewRequest(http.MethodGet, "/v1/users/token", nil)
+	w := httptest.NewRecorder()
+
+	r.SetBasicAuth("unknown@example.com", "some-password")
+	ut.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to deny tokens to unknown users.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen fetching a token with an unrecognized email.", testID)
+		{
+			if w.Code != http.StatusNotFound {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 404 for the response : %v", tests.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 404 for the response.", tests.Success, testID)
+		}
+	}
+}
+
+func (ut *UserTests) getToken200(t *testing.T) {
+	r := httptest.NewRequest(http.MethodGet, "/v1/users/token", nil)
+	w := httptest.NewRecorder()
+
+	r.SetBasicAuth("admin@example.com", "gophers")
+	ut.app.ServeHTTP(w, r)
+
+	t.Log("Given the need to issues tokens to known users.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen fetching a token with valid credentials.", testID)
+		{
+			if w.Code != http.StatusOK {
+				t.Fatalf("\t%s\tTest %d:\tShould receive a status code of 200 for the response : %v", tests.Failed, testID, w.Code)
+			}
+			t.Logf("\t%s\tTest %d:\tShould receive a status code of 200 for the response.", tests.Success, testID)
+
+			var got struct {
+				Token string `json:"token"`
+			}
+			if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to unmarshal the response : %v", tests.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to unmarshal the response.", tests.Success, testID)
+
+			// TODO(jlw) Should we ensure the token is valid?
+		}
+	}
+}
--- business/data/tests/tests.go
+++ business/data/tests/tests.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"github.com/ardanlabs/service/business/data/schema"
+	"github.com/ardanlabs/service/business/data/store/user"
 	"github.com/ardanlabs/service/business/sys/auth"
 	"github.com/ardanlabs/service/business/sys/database"
 	"github.com/ardanlabs/service/foundation/docker"
@@ -135,6 +136,24 @@ func NewIntegration(t *testing.T, dbc DBContainer) *Test {
 	}
 
 	return &test
+}
+
+// Token generates an authenticated token for a user.
+func (test *Test) Token(email, pass string) string {
+	test.t.Log("Generating token for test ...")
+
+	store := user.NewStore(test.Log, test.DB)
+	claims, err := store.Authenticate(context.Background(), time.Now(), email, pass)
+	if err != nil {
+		test.t.Fatal(err)
+	}
+
+	token, err := test.Auth.GenerateToken(claims)
+	if err != nil {
+		test.t.Fatal(err)
+	}
+
+	return token
 }
 
 // StringPointer is a helper to get a *string from a string. It is in the tests
