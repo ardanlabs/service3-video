a7f8c0ec443fcd9287d13f62e89a72018d12e859
William Kennedy <bill@ardanlabs.com>
2021-09-09 11:42:09 -0400

    13.2-TestingData

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -17,9 +17,9 @@ import (
 	"github.com/ardanlabs/service/business/sys/auth"
 	"github.com/ardanlabs/service/business/sys/database"
 	"github.com/ardanlabs/service/foundation/keystore"
+	"github.com/ardanlabs/service/foundation/logger"
 	"go.uber.org/automaxprocs/maxprocs"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 /*
@@ -31,7 +31,7 @@ var build = "develop"
 func main() {
 
 	// Construct the application logger.
-	log, err := initLogger("SALES-API")
+	log, err := logger.New("SALES-API")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -235,21 +235,4 @@ func run(log *zap.SugaredLogger) error {
 	}
 
 	return nil
-}
-
-func initLogger(service string) (*zap.SugaredLogger, error) {
-	config := zap.NewProductionConfig()
-	config.OutputPaths = []string{"stdout"}
-	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	config.DisableStacktrace = true
-	config.InitialFields = map[string]interface{}{
-		"service": service,
-	}
-
-	log, err := config.Build()
-	if err != nil {
-		return nil, err
-	}
-
-	return log.Sugar(), nil
 }
---
+++ business/data/tests/tests.go
@@ -0,0 +1,96 @@
+// Package tests contains supporting code for running tests.
+package tests
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/ardanlabs/service/business/data/schema"
+	"github.com/ardanlabs/service/business/sys/database"
+	"github.com/ardanlabs/service/foundation/docker"
+	"github.com/ardanlabs/service/foundation/logger"
+	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
+)
+
+// Success and failure markers.
+const (
+	Success = "\u2713"
+	Failed  = "\u2717"
+)
+
+// DBContainer provides configuration for a container to run.
+type DBContainer struct {
+	Image string
+	Port  string
+	Args  []string
+}
+
+// NewUnit creates a test database inside a Docker container. It creates the
+// required table structure but the database is otherwise empty. It returns
+// the database to use as well as a function to call at the end of the test.
+func NewUnit(t *testing.T, dbc DBContainer) (*zap.SugaredLogger, *sqlx.DB, func()) {
+	r, w, _ := os.Pipe()
+	old := os.Stdout
+	os.Stdout = w
+
+	c := docker.StartContainer(t, dbc.Image, dbc.Port, dbc.Args...)
+
+	db, err := database.Open(database.Config{
+		User:       "postgres",
+		Password:   "postgres",
+		Host:       c.Host,
+		Name:       "postgres",
+		DisableTLS: true,
+	})
+	if err != nil {
+		t.Fatalf("Opening database connection: %v", err)
+	}
+
+	t.Log("Waiting for database to be ready ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := schema.Migrate(ctx, db); err != nil {
+		docker.DumpContainerLogs(t, c.ID)
+		docker.StopContainer(t, c.ID)
+		t.Fatalf("Migrating error: %s", err)
+	}
+
+	if err := schema.Seed(ctx, db); err != nil {
+		docker.DumpContainerLogs(t, c.ID)
+		docker.StopContainer(t, c.ID)
+		t.Fatalf("Seeding error: %s", err)
+	}
+
+	log, err := logger.New("TEST")
+	if err != nil {
+		t.Fatalf("logger error: %s", err)
+	}
+
+	// teardown is the function that should be invoked when the caller is done
+	// with the database.
+	teardown := func() {
+		t.Helper()
+		db.Close()
+		docker.StopContainer(t, c.ID)
+
+		log.Sync()
+
+		w.Close()
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		os.Stdout = old
+		fmt.Println("******************** LOGS ********************")
+		fmt.Print(buf.String())
+		fmt.Println("******************** LOGS ********************")
+	}
+
+	return log, db, teardown
+}
